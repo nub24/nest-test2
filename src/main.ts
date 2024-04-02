ba491bbc02dc670ac839500d7eb83683fc94609f
nub24 <nub24@rambler.ru>
2024-04-02 08:57:48 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Lesson on advanced backend')
+    .setDescription('Documentation REST API')
+    .setVersion('0.0.1')
+    .addTag('Fedorov Alex')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,13 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { Role } from 'src/roles/roles.model';
+import { UserRoles } from 'src/roles/user-roles.model';
 
 interface UserCreationAttr {
   email: string;
@@ -7,6 +16,7 @@ interface UserCreationAttr {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttr> {
+  @ApiProperty({ example: '1', description: 'Идентификатор' })
   @Column({
     type: DataType.INTEGER,
     unique: true,
@@ -15,15 +25,22 @@ export class User extends Model<User, UserCreationAttr> {
   })
   id: number;
 
+  @ApiProperty({ example: 'user@email.ru', description: 'Электронная почта' })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
 
+  @ApiProperty({ example: '12345', description: 'Пароль пользователя' })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
 
+  @ApiProperty({ example: 'true', description: 'Забанен пользователь или нет' })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
 
+  @ApiProperty({ example: 'Какая-то причина', description: 'Причина бана' })
   @Column({ type: DataType.STRING, allowNull: true })
   banReason: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
 }
