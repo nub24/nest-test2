9b73ebe58ed075e19dad10d4f214490e9f449a31
nub24 <nub24@rambler.ru>
2024-04-03 12:56:53 +0700

    fix

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class AddroleDto {
   @ApiProperty({ example: 'Administrator', description: 'Значение роли' })
+  @IsString({ message: 'Должно быть строкой' })
   readonly value: string;
   @ApiProperty({
     example: '5',
     description: 'Id пользователя, которому добавляется роль',
   })
+  @IsNumber({}, { message: 'Должно быть числом' })
   readonly userId: number;
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({ example: 'user@email.ru', description: 'Электронная почта' })
+  @IsString({ message: 'Поле email должно быть строкой' })
+  @IsEmail({}, { message: 'Некорректный адрес электронной почты' })
   readonly email: string;
   @ApiProperty({ example: '123456', description: 'Пароль пользователя' })
+  @IsString({ message: 'Поле password должно быть строкой' })
+  @Length(6, 16, { message: 'Пароль должен быть длиной от 6 до 16 символов' })
   readonly password: string;
 }
